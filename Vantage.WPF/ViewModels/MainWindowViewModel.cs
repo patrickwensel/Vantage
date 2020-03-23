78d02c6c5b691ef68538337bb4492e695f348806
Javed Kalaniya <javedappdevelopment@gmail.com>
2020-03-23 14:09:52 +0530

    Fixed password textbox does not visible fully on screen

---
+++ Vantage.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vantage.WPF.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
