40a64b297f5f414c9688a973f9af11fcb19f8d45
Javed Kalaniya <javedappdevelopment@gmail.com>
2020-04-02 12:57:49 +0530

    Some UI changes in Training Report screen and xaml document formatting

---
+++ Vantage.WPF/Converters/AttemptedCompletedLessonCountConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using Vantage.Common.Models;
+
+namespace Vantage.WPF.Converters
+{
+    public class AttemptedCompletedLessonCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return "0/0";
+
+            if (value is List<Attempt>)
+            {
+                List<Attempt> attempts = value as List<Attempt>;
+                return $"{attempts.Count(x => x.IsComplete)}/{attempts.Count}";
+            }
+
+            return "0/0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
