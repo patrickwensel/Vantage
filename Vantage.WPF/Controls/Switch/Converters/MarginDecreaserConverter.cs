75820a6d9421d3ff3cd6057cc24295aa21dbd680
Patrick Wensel <pwensel@xenatech.com>
2020-04-04 18:26:20 +0530

    Update switch design

---
+++ Vantage.WPF/Controls/Switch/Converters/MarginDecreaserConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Vantage.WPF.Controls.Switch.Converters
+{
+    public class MarginDecreaserConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double thumbSize = (double)value;
+            double decreaseAmount = 4;
+            if (parameter != null)
+                decreaseAmount = double.Parse(parameter.ToString());
+
+            return thumbSize - decreaseAmount;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 4;
+        }
+    }
+}
