6840424954c8b1f04fcd56cc6aa0849176d4c81e
Patrick Wensel <pwensel@xenatech.com>
2020-08-06 13:39:52 +0530

    Added DatabaseGenerated Attribute in Product Table

--- Vantage.Common/Models/Product.cs
+++ Vantage.Common/Models/Product.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Vantage.Common.Models
 {
     public class Product
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductID { get; set; }
 
         public string Name { get; set; }
